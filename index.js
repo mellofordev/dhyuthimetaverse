b26b9f4d4f30125948abaf3d617c64fa23a135d1
sreedhar <sreedharks2003@gmail.com>
2022-09-27 18:23:33 +0530

    server bug fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express =require('express');
 const path=require('path');
-const server = express();
+const server = express.Router();
 server.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'public/index.html'))
 })
