3b37764ac5d16eb7da32b346cc596be71ed57a02
ajithrt007 <ajithrt170@gmail.com>
2022-10-06 18:49:47 +0530

    Added a tag to d-island and added scroll into view code

---
+++ public/pkgs/scrollToPosition.js
@@ -0,0 +1,26 @@
+//back-to-top button 
+const home = document.querySelector("#home");
+const goToHome = () => {
+    home.scrollIntoView({behavior: "smooth", block: "start", inline: "start"});
+};
+
+
+//go to about section code
+const about = document.querySelector(".about");
+const goToAbout = () => {
+    about.scrollIntoView({behavior: "smooth", block: "start", inline: "start"});
+};
+
+
+//go to teams section code
+const campusambassador = document.querySelector(".campusambassador");
+const goToCA = () => {
+    campusambassador.scrollIntoView({behavior: "smooth", block: "start", inline: "start"});
+};
+
+
+//go to events section code
+const contact = document.querySelector(".footer");
+const goToContact = () => {
+    contact.scrollIntoView({behavior: "smooth", block: "start", inline: "start"});
+};
